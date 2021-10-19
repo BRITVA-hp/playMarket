16c445117d8c74e51fbcd7afd4027ce7713733d0
BRITVA-hp <borbritvahp@gmail.com>
2021-10-20 01:04:53 +0400

    almost all

--- js/script.js
+++ js/script.js
@@ -10,7 +10,7 @@ document.addEventListener('DOMContentLoaded', () => {
               burger = document.querySelector('.header__burger');
 
         trigger.addEventListener('click', function() {
-            this.classList.toggle('header__top__loup--active');
+            trigger.classList.toggle('header__top__loup--active');
             input.classList.toggle('visible');
             logo.classList.toggle('visible');
             button.classList.toggle('visible');
@@ -30,13 +30,13 @@ document.addEventListener('DOMContentLoaded', () => {
         for(let i = 0; i < parent.children.length; i++) {
             if (parent.children[i] === element) {
                 a = 1;
+                return;
             }
             if (parent.children.length > 0) {
                 recurcy(parent.children[i], element);
             }
             
         }
-        return false;
     }
 
     // Функция для ховера в меню на десктопе
@@ -78,13 +78,17 @@ document.addEventListener('DOMContentLoaded', () => {
     function burger() {
 
         const burger = document.querySelector('.header__burger'),
-              main = document.querySelector('.main'),
-              menu = document.querySelector('.menu');
+              containerMain = document.querySelector('.container-main'),
+              menu = document.querySelector('.menu'),
+              footer = document.querySelector('.footer'),
+              buttonTel = document.querySelector('.button__tel');
 
         burger.addEventListener('click', () => {
-            main.classList.toggle('main--active');
+            containerMain.classList.toggle('container-main--active');
             menu.classList.toggle('menu--visible');
-            if ( menu.classList.contains('menu--visible')) {
+            footer.classList.toggle('footer--active');
+            buttonTel.classList.toggle('button__tel--active');
+            if ( containerMain.classList.contains('container-main--active')) {
                 document.body.style.overflow = 'hidden';
             } else {
                 document.body.style.overflow = '';
@@ -95,6 +99,114 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     burger();
+
+    // Слайдер
+
+    function slider() { 
+        const prev = document.querySelector('.slider__button--prev'),
+              next = document.querySelector('.slider__button--next'),
+              field = document.querySelector('.slider__field'),
+              sliderRow = document.querySelector('.slider__row');
+
+        let translate = 0,
+            interval;
+
+        next.addEventListener('mousedown', (e) => {
+            e.preventDefault();
+            e.stopPropagation();
+
+            if (e.which == 2 || e.which == 3) {
+                return;
+            }
+            
+            interval = setInterval(() => {
+                if (translate >= (field.scrollWidth + 2 * next.scrollWidth) - sliderRow.scrollWidth) {
+                    return;
+                }
+                field.style.transform = `translateX(-${translate+=20}px)`;
+            }, 50);
+        });
+
+        next.addEventListener('mouseup', () => {
+            clearInterval(interval);
+        });
+        next.addEventListener('mouseout', () => {
+            clearInterval(interval);
+        });
+
+        prev.addEventListener('mousedown', (e) => {
+            e.preventDefault();
+            e.stopPropagation();
+
+            if (e.which == 2 || e.which == 3) {
+                return;
+            }
+
+            interval = setInterval(() => {
+                if (translate < 0) {
+                    return;
+                }
+                field.style.transform = `translateX(-${translate-=20}px)`;
+            }, 50);
+        });
+
+        prev.addEventListener('mouseup', () => {
+            clearInterval(interval);
+        });
+        prev.addEventListener('mouseout', () => {
+            clearInterval(interval);
+        });
+    }
+
+    slider();
+
+    // Видео
+
+    function video() {
+        const play = document.querySelectorAll('.slider__play'),
+              video = document.querySelector('#video'),
+              modalVideo = document.querySelector('.modal-video');
+
+        play.forEach(item => {
+            item.addEventListener('click', () => {
+                modalVideo.classList.add('modal--visible');
+                document.body.style.overflow = 'hidden';
+            });
+        });
+
+        modalVideo.addEventListener('click', (e) => {
+            if (e.target.classList.contains('modal--visible')) {
+                modalVideo.classList.remove('modal--visible');
+                video.pause();
+                document.body.style.overflow = '';
+            }
+        });
+    }
+
+    video();
+
+    // Подробнее/скрыть
+
+    function openClose(button, content, buttonActiveClass, buttonText, buttonTextActive) {
+        const button_ = document.querySelector(button),
+              content_ = document.querySelector(content);
+
+        button_.addEventListener('click', function() {
+            this.classList.toggle(buttonActiveClass);
+            if (this.classList.contains(buttonActiveClass)) {
+                this.textContent = buttonTextActive;
+                content_.style.maxHeight = `${content_.scrollHeight}px`;
+                this.previousElementSibling.style.display = 'none';
+            } else {
+                this.textContent = buttonText;
+                content_.style.maxHeight = '';
+                this.previousElementSibling.style.display = 'block';
+            }
+        });
+    }
+
+    openClose('.description__button--1 span', '.description__text', 'description__button--active', 'подробнее...', 'скрыть');
+    openClose('.description__button--2 span', '.news__text', 'description__button--active', 'подробнее...', 'скрыть');
 
 
 });
